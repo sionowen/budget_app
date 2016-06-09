b02bcd4e57c9c527f45b018ae13fd8f0d09e9e2d
sionowen <sionowen34@gmail.com>
2016-06-09 16:24:01 -0500

    created the front end of a working calendar using moment.js

--- README.md
+++ README.md
@@ -2,9 +2,36 @@
 
 npm install to get started.
 
+Using Postgres create the following tables and constraints
+
+CREATE TABLE total (
+	id SERIAL PRIMARY KEY,
+	balance float,
+	user_id int
+)
 
 CREATE TABLE users (
  id SERIAL PRIMARY KEY,
  username VARCHAR(100) NOT NULL UNIQUE,
  password VARCHAR(120) NOT NULL
 );
+
+CREATE TABLE events (
+	id SERIAL PRIMARY KEY,
+	transaction character varying(30),
+	name character varying(100),
+	description character varying(500),
+	amount float NOT NULL,
+	recurring boolean,
+	frequency character varying(100),
+	execute_date character varying(100),
+	user_id int NOT NULL
+)
+
+ALTER TABLE events
+   ADD CONSTRAINT check_frequency
+   CHECK (frequency = 'weekly' OR frequency = 'bi-weekly' OR frequency = 'monthly')
+
+   ALTER TABLE events
+   ADD CONSTRAINT check_types
+   CHECK (transaction = 'income' OR transaction = 'expense');
--- server/public/scripts/controllers/homeController.js
+++ server/public/scripts/controllers/homeController.js
@@ -1,8 +1,91 @@
 myApp.controller('HomeController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
   console.log('home controller running');
   $scope.message = "Home Controller!";
-
+  $scope.day = moment();
   $scope.user_id= {};
+  var savedMonth = undefined;
+
+  //notes
+  // console.log('scope.day', $scope.day);
+  // console.log('start of',moment().startOf('month'));
+  // console.log('.day 0 gets beginning of week', moment().day(0));
+  // console.log('end of month', moment().endOf('month'));
+
+
+  var monthArray = [];
+//  var daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var start = removeTime(moment().startOf('month'));
+
+  console.log('thisMonth', start.month());
+
+  buildMonth(start, start);
+  console.log('month array', monthArray);
+
+
+
+  function buildMonth(start, month){
+    var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
+    monthArray = [];
+    while (!done) {
+        monthArray.push({ days: buildWeek(date.clone(), month) });
+        date.add(1, "w");
+        done = count++ > 2 && monthIndex !== date.month();
+        monthIndex = date.month();
+    }
+    var filterForBadWeek = [];
+    filterForBadWeek = _.filter(monthArray[0].days, {'number': 1 })
+    console.log("bad week pre if", filterForBadWeek);
+    if (filterForBadWeek.length == 0){
+      monthArray.shift();
+      console.log('this ran');
+    }
+    $scope.monthArray = monthArray;
+    $scope.month = moment().month(start.month() +1).format('MMMM');
+    console.log('Currently viewed month Array', $scope.monthArray);
+  }
+
+
+  function buildWeek(date, month){
+    var days = [];
+    for (var i = 0; i < 7; i++) {
+        days.push({
+            name: date.format("dd").substring(0, 2),
+            number: date.date(),
+            isCurrentMonth: date.month() === month.month(),
+            isToday: date.isSame(new Date(), "day"),
+            date: moment(date, "MM-DD-YYYY"),
+            transactions: []
+        });
+        date = date.clone();
+        date.add(1, "d");
+    }
+    return days;
+  }
+
+  function removeTime(date) {
+      return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+  }
+
+  $scope.next = function() {
+      if(savedMonth === undefined){
+        savedMonth = start.clone();
+      }
+      var currentMonth = savedMonth.month()
+      console.log(savedMonth.month());
+      var next = removeTime(moment().month(savedMonth.month() + 2).date(0));
+      savedMonth = next.clone()
+      console.log('next month', next);
+      buildMonth(next, next);
+  };
+
+  $scope.previous = function() {
+      var previous = scope.month.clone();
+      removeTime(previous.month(previous.month()-1).date(1));
+      scope.month.month(scope.month.month()-1);
+      buildMonth(scope, previous, scope.month);
+  };
+
+
   getUser();
 
   function getUser() {
